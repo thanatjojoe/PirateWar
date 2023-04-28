694942ae20f6a0d73ac3a02cfcaaaaf235c63a9e
thanatjojoe <thanatjojoe12@gmail.com>
2023-04-28 21:47:37 +0700

    add player move to origin spot

--- unityPirate/Assets/Code/Player.cs
+++ unityPirate/Assets/Code/Player.cs
@@ -6,7 +6,7 @@ public class Player : MonoBehaviour
 {
     public HealthBar healthBar;
     private bool isMovingBack;
-    
+    public Vector3 originalPosition;
     PlayerConstrutor player = new PlayerConstrutor(100,10,10);
     
     
@@ -15,24 +15,41 @@ public class Player : MonoBehaviour
     {
        
         player.currentHealth = player.Hp;
+        isMovingBack = false;
     }
     
     void Update()
     {
+        if (isMovingBack == true)
+        {
+            // Move the enemy back to their original position
+            float step = 5f * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, originalPosition, step);
 
+            // Check if the enemy has reached their original position
+            if (transform.position == originalPosition)
+            {
+                isMovingBack = false;
+            }
+        }
+        if (BulletEnemy.playerMoveToOrigin == true)
+        {
+            isMovingBack = true;
+            BulletEnemy.playerMoveToOrigin = false;
+        }
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
         
-            if (other.gameObject.tag == "Bullet")
-            {
-                player.RecieveDmg(10);
-                healthBar.SetHealth(player.currentHealth);
-            }
-            if (other.gameObject.tag == "Water")
-            {
-                StartCoroutine(StartTimer(2));
-            }
+        if (other.gameObject.tag == "Bullet")
+        {
+            player.RecieveDmg(10);
+            healthBar.SetHealth(player.currentHealth);
+        }
+        if (other.gameObject.tag == "Water")
+        {
+            StartCoroutine(StartTimer(2));
+        }
     } 
     IEnumerator StartTimer(int timeRemaining)
     {
@@ -40,7 +57,6 @@ public class Player : MonoBehaviour
         {
             yield return new WaitForSeconds(1);
         }
-
         Destroy(gameObject);
     }
 }
